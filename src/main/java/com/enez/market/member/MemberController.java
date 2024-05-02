213e2cc9ef3bb4a4fce2830999f9c0408af19a30
jinwook <sherr2@naver.com>
2024-05-02 16:53:47 +0900

    2024-05-02 member dao,controller,DTO 추가

--- src/main/java/com/enez/market/member/MemberController.java
+++ src/main/java/com/enez/market/member/MemberController.java
@@ -133,6 +133,7 @@ public class MemberController {
 		HttpSession hs = request.getSession();
 		hs.removeAttribute("dto");
 		hs.removeAttribute("loginstate");
+		hs.removeAttribute("member_id");
 		hs.setAttribute("loginstate", false);
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter piw = response.getWriter();
@@ -349,6 +350,7 @@ public class MemberController {
 		hs = request.getSession();
 		hs.removeAttribute("dto");
 		hs.removeAttribute("loginstate");
+		hs.removeAttribute("member_id");
 		hs.setAttribute("loginstate", false);
 		hs.invalidate();// 세션을 완전히 종료하는 함수
 		
@@ -381,6 +383,30 @@ public class MemberController {
 		}
 		
 		return "redirect:memberlist";
+	}
+	
+	
+	@RequestMapping(value = "mypage2")
+	public String mypage(HttpServletRequest request,HttpSession hs,Model mo) {
+		String member_id = (String)hs.getAttribute("member_id");
+		System.out.println(member_id);
+		Service ss = sqlSession.getMapper(Service.class);
+	
+		MemberDTO member = ss.select(member_id);
+		mo.addAttribute("member", member);
+
+		return "mypage2";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "introsave")
+	public String intro(HttpServletRequest request,HttpSession hs,Model mo) {
+		String member_id = (String)hs.getAttribute("member_id");
+		String intro = request.getParameter("intro");
+		Service ss = sqlSession.getMapper(Service.class);
+		ss.itrsave(member_id,intro);
+		
+		return "";
 	}
 
 
